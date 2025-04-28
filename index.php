9b312f835d8e24a3a8c0d2ce08258118380bd112
Enno Hyttrek <85670244+ehy-training@users.noreply.github.com>
2025-04-28 23:09:24 +0200

    Version: 0.1.1

--- index.php
+++ index.php
@@ -1,23 +1,49 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WP Dev Theme</title>
     <?php wp_head(); ?>
 </head>
+
 <body>
     <?php wp_body_open(); ?>
     <header>
-        <h1>Mein erstes Custom WP Theme</h1>
-        <!-- Header-Inhalt -->
+        <h1><?php bloginfo('name'); ?></h1>
+        <p><?php bloginfo('description'); ?></p>
     </header>
+
     <main>
-        <!-- Hauptinhalt -->
+
+    <!-- Der WP Loop -->
+        <?php
+        if (have_posts()):
+            while (have_posts()):
+                the_post();
+                ?>
+
+                <h2><?php the_title(); ?></h2>
+
+                <?php
+                the_content();
+            endwhile;
+        else:
+            ?>
+            <p><?php _e('Sorry, no content available.', 'theme-dev-classic'); ?></p>
+            <?php
+        endif;
+        ?>
+
     </main>
+
     <footer>
-        <!-- Footer-Inhalt -->
+        <p>
+            &copy; 2024 - <?php echo date('Y'); ?> Enno Hyttrek
+        </p>
     </footer>
     <?php wp_footer(); ?>
 </body>
+
 </html>
