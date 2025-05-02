d2091647acc80f5de0fcc2e9664c22777b59816b
Enno Hyttrek <85670244+ehy-training@users.noreply.github.com>
2025-05-02 11:35:00 +0200

    Version: 0.1.7
    - Improve style.css

--- functions.php
+++ functions.php
@@ -27,3 +27,33 @@ function theme_dev_classic_register_menus() {
     ]);
 }
 add_action('after_setup_theme', 'theme_dev_classic_register_menus');
+
+// Theme Support Funktionen
+function theme_dev_classic_setup() {
+    // Unterstützung für Beitragsbilder
+    add_theme_support('post-thumbnails');
+    // Unterstützung für HTML5-Markup
+    add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption']);
+    // Unterstützung für dynamische Titel-Tags
+    add_theme_support('title-tag');
+    // Unterstützung für benutzerdefiniertes Logo
+    add_theme_support('custom-logo', [
+        'height' => 100,
+        'width' => 400,
+        'flex-height' => true,
+        'flex-width' => true,
+    ]);
+    // Unterstützung für benutzerdefinierten Hintergrund
+    add_theme_support('custom-background', [
+        'default-color' => 'ffffff',
+        'default-image' => '',
+    ]);
+    // Unterstützung für Editor-Styles
+    add_theme_support('editor-styles');
+    add_editor_style('css/editor-style.css');
+    // Unterstützung für responsive Embeds
+    add_theme_support('responsive-embeds');
+    // Unterstützung für WooCommerce
+    add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'theme_dev_classic_setup');
--- header.php
+++ header.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Dev Theme</title>
+    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
     <nav class="main-nav nav-bar">
